9ef102346bc7dd53c013943b4568915e31d9ecb1
Marcin Bożenta <m.bozenta@neurosys.com>
2018-06-03 17:50:45 +0200

    added asteroid spawning

--- Game/Scripts/asteroidController.cs
+++ Game/Scripts/asteroidController.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class asteroidController : MonoBehaviour
 {
 
-  public float maxVelocity = 10;
+  public float maxVelocity = 1;
+  public float offset = 0.2f;
 
   void Start()
   {
-    GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(0, maxVelocity), 0.0f, Random.Range(0, maxVelocity));
-    Debug.Log(GetComponent<Rigidbody>());
+    if (GameObject.FindGameObjectWithTag("MainCamera").GetComponent<cameraController>().GetLockTag() == "earth")
+    {
+      Vector3 direction = new Vector3(
+        GameObject.FindGameObjectWithTag("earth").transform.position.x - transform.position.x,
+        0.0f,
+        GameObject.FindGameObjectWithTag("earth").transform.position.z - transform.position.z
+      ).normalized;
+      GetComponent<Rigidbody>().velocity = new Vector3(Random.Range((direction.x - offset) * maxVelocity, (direction.x + offset) * maxVelocity), 0.0f, Random.Range((direction.z - offset) * maxVelocity, (direction.z + offset) * maxVelocity));
+    }
+    if (GameObject.FindGameObjectWithTag("MainCamera").GetComponent<cameraController>().GetLockTag() == "mars")
+    {
+      Vector3 direction = new Vector3(
+        GameObject.FindGameObjectWithTag("mars").transform.position.x - transform.position.x,
+        0.0f,
+        GameObject.FindGameObjectWithTag("mars").transform.position.z - transform.position.z
+      ).normalized;
+      GetComponent<Rigidbody>().velocity = new Vector3(Random.Range((direction.x - offset) * maxVelocity, (direction.x + offset) * maxVelocity), 0.0f, Random.Range((direction.z - offset) * maxVelocity, (direction.z + offset) * maxVelocity));
+    }
   }
 }
